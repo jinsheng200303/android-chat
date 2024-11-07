ac45545c68c148446f49627d551b4d51e0379f70
jinsheng <2247991411@qq.com>
2024-11-07 14:39:29 +0800

    第二次本地部署

--- chat/src/main/java/cn/wildfire/chat/app/AppService.java
+++ chat/src/main/java/cn/wildfire/chat/app/AppService.java
@@ -54,7 +54,7 @@ public class AppService implements AppServiceProvider {
      * <br>
      * <br>
      */
-    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释，http 前缀不能省略*/ = "http://118.178.243.70:8888";
+    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释，http 前缀不能省略*/ = "http://47.98.147.62:8888";
 //    public static String APP_SERVER_ADDRESS/*请仔细阅读上面的注释*/ = "https://app.wildfirechat.net";
 
     private AppService() {
